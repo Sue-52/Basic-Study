b38df249133c8b44324f8abce7e5e8e2c34fd5d4
Sue-52 <1219243947@qq.com>
2021-07-09 08:55:51 +0800

    submit

--- Sakura-Template/vite.config.ts
+++ Sakura-Template/vite.config.ts
@@ -1,13 +1,28 @@
+/*
+ * @Author: Edmond℡优格·索托斯T_T～♥
+ * @Date: 2021-06-29 20:51:50
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2021-07-09 08:49:39
+ * @FilePath: \Basic-Study\Sakura-Template\vite.config.ts
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue';
 // 模块化配置
-// const path = require('path');
+import * as path from 'path';
+// 绝对路径
+var __dirname: string;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  resolve: {
+    alias: {
+      // 配置别名
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   css: {
     preprocessorOptions: {
       scss: {
