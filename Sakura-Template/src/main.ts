e6469f28c49e149cda4084180e78d85829bc3387
Sue-52 <1219243947@qq.com>
2021-07-12 15:49:02 +0800

    submit：promise, async

--- Sakura-Template/src/main.ts
+++ Sakura-Template/src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Edmond℡优格·索托斯T_T～♥
  * @Date: 2021-06-29 20:51:50
  * @LastEditors: OBKoro1
- * @LastEditTime: 2021-07-07 11:44:52
+ * @LastEditTime: 2021-07-12 11:47:16
  * @FilePath: \Basic-Study\Sakura-Template\src\main.ts
  */
 import { createApp } from 'vue'
@@ -13,17 +13,12 @@ import "./utils/resetCSS/reset.css"
 import router from "./routes/routes";
 // 挂载vuex
 import store from "./store/store"
-// 引入 ant-design-vue ui库
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
 // 挂载vuex
 app.use(store);
 // 挂载路由
 app.use(router);
-// 挂载antd
-app.use(Antd);
 
 // 实例化#app
 app.mount('#app')
